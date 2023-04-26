ddccd2a469c4b2aa5cd5851c93e0d0c4352d24ba
a-liukun <995340133@qq.com>
2023-04-26 19:37:27 +0800

    补充头文件

--- CGImysql/sql_connection_pool.h
+++ CGImysql/sql_connection_pool.h
@@ -1,5 +1,13 @@
 #pragma once
-
+#include <stdio.h>
+#include <list>
+#include <mysql/mysql.h>
+#include <error.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include "../lock/locker.h"
+#include "../log/log.h"
 using namespace std;
 
 class connection_pool
